9bacf4a44870037f386db5ccfd8f7b92783d95bd
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-02-23 14:39:19 +0900

    [Gold V] Title: 강의실 배정, Time: 504 ms, Memory: 148584 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,22 +1,34 @@
 import heapq
 
-rooms = 0
 
-end_times = []
-total = []
-n = int(input())
-for i in range(n):
-    total.append(list(map(int, input().split())))
+def input_schedule():
+    total = []
+
+    for i in range(int(input())):
+        total.append(list(map(int, input().split())))
+    total.sort()
+
+    return total
 
 
-total.sort()
-loop = 0
-for start, end in total:
-    if end_times and end_times[0] <= start:
-        heapq.heappop(end_times)
-        heapq.heappush(end_times, end)
-    else:
-        heapq.heappush(end_times, end)
-        rooms += 1
+def min_rooms(total):
+    rooms = 0
+    end_times = []
+    for start, end in total:
+        if end_times and end_times[0] <= start:
+            heapq.heappop(end_times)
+            heapq.heappush(end_times, end)
+        else:
+            heapq.heappush(end_times, end)
+            rooms += 1
 
-print(rooms)
+    return rooms
+
+
+def main():
+    total_schedule = input_schedule()
+    print(min_rooms(total_schedule))
+
+
+if __name__ == "__main__":
+    main()
