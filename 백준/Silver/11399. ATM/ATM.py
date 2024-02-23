b54a586c4c2326ed86bf10534cca3502f1255906
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-02-23 18:18:45 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,13 +1,24 @@
-
-n = int(input())
-withdrawal = list(map(int, input().split()))
-withdrawal.sort()
-
-wait_time = 0
-sum_time = 0
-for i in range(n):
-    wait_time = wait_time + withdrawal[i]
-    sum_time += wait_time
+def sort_wait_time():
+    n = int(input())
+    withdrawal = list(map(int, input().split()))
+    withdrawal.sort()
+    return withdrawal
 
 
-print(sum_time)
+def total_wait_time(withdrawal):
+    wait_time = 0
+    sum_time = 0
+    for i in range(len(withdrawal)):
+        wait_time = wait_time + withdrawal[i]
+        sum_time += wait_time
+
+    return sum_time
+
+
+def main():
+    withdrawal = sort_wait_time()
+    print(total_wait_time(withdrawal))
+
+
+if __name__ == "__main__":
+    main()
