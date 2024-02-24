c04dd7218656e1933d4ca8433e6644f1e18bba99
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-02-24 13:33:44 +0900

    [Silver II] Title: 나무 자르기, Time: 452 ms, Memory: 257900 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,25 @@
+
+def cal(trees, height):
+    while True:
+        sum_tree = 0
+
+        for tree in trees:
+            cut = tree - height
+            if cut <= 0:
+                break
+            sum_tree += cut
+
+        if sum_tree >= m:
+            return height
+        minus = (m - sum_tree)//len(trees)
+        if minus < 1:
+            minus = 1
+        height -= minus
+
+
+num_tree, m = map(int, input().split())
+trees = list(map(int, input().split()))
+trees.sort(reverse=True)
+
+height = trees[0]
+print(cal(trees, height))
