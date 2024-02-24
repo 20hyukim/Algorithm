2cf3dcb2ac328460b17d161b53ab1cceff061a38
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-02-25 02:12:41 +0900

    [Silver II] Title: 나무 자르기, Time: 444 ms, Memory: 257916 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,5 +1,4 @@
-
-def cal(trees, height):
+def cal(trees, height, num_tree, m):
     while True:
         sum_tree = 0
 
@@ -11,17 +10,21 @@ def cal(trees, height):
 
         if sum_tree >= m:
             return height
-        minus = (m - sum_tree)//len(trees)
+        minus = (m - sum_tree)//num_tree + 1
         if minus < 1:
             minus = 1
         height -= minus
 
 
-num_tree, m = map(int, input().split())
-trees = list(map(int, input().split()))
-trees.sort(reverse=True)
+def main():
+    num_tree, m = map(int, input().split())
+    trees = list(map(int, input().split()))
+    trees.sort(reverse=True)
+    height = trees[0]
 
-height = trees[0]
-minus = m//len(trees)
-print(cal(trees, height-minus))
+    start_point = m // len(trees)
+    print(cal(trees, height - start_point, num_tree, m))
 
+
+if __name__ == "__main__":
+    main()
