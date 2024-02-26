79156687ca11de82549b3489199ab35a51f455ea
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-02-26 22:16:56 +0900

    [Gold IV] Title: 최단경로, Time: 584 ms, Memory: 191372 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,7 +1,7 @@
 import heapq
 
 INF = int(1e9)
-
+ 
 
 def findCheapestPrice(n, src, nodes):
     dist = [INF] * (n + 1)
@@ -31,16 +31,27 @@ def findCheapestPrice(n, src, nodes):
     return dist
 
 
-n, e = map(int, input().split())
-start = int(input())
-nodes = []
-for _ in range(e):
-    nodes.append(list(map(int, input().split())))
+def input_nodes(n, e):
+    nodes = []
+    for _ in range(e):
+        nodes.append(list(map(int, input().split())))
+    return nodes
 
-dist = findCheapestPrice(n, start, nodes)
 
-for i in range(1, n + 1):
-    if dist[i] == INF:
-        print("INF")
-    else:
-        print(dist[i])
+def print_nodes(dist, n):
+    for i in range(1, n + 1):
+        if dist[i] == INF:
+            print("INF")
+        else:
+            print(dist[i])
+
+
+def main():
+    n, e = map(int, input().split())
+    start = int(input())
+    dist = findCheapestPrice(n, start, input_nodes(n, e))
+    print_nodes(dist, n)
+
+
+if __name__ == "__main__":
+    main()
