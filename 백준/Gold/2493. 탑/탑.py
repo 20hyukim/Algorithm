b317b3c57f002803b30b7c21ae57f12bee89c94e
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-09-10 21:57:05 +0900

    [Gold V] Title: 탑, Time: 356 ms, Memory: 242204 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,34 @@
+def laser(n):
+    hit = [0] * n
+    b_shoot = []
+    temp_shoot = list(map(int, input().split()))
+    for i in range(len(temp_shoot)):
+        b_shoot.append([temp_shoot[i], i + 1])
+    a_shoot = []
+    while b_shoot:
+        n_h, n_i = b_shoot.pop()
+
+        while a_shoot:
+            if n_h > a_shoot[-1][0]:
+                o_h, o_i = a_shoot.pop()
+                hit[o_i - 1] = n_i
+            else:
+                break
+
+        a_shoot.append([n_h, n_i])
+
+    return hit
+
+
+def strize(hit):
+    str_hit = []
+    for h in hit:
+        str_hit.append(str(h))
+
+    return str_hit
+
+
+if __name__ == "__main__":
+    hit = laser(int(input()))
+    str_hit = strize(hit)
+    print(" ".join(str_hit))
