886483ed615c55c6d2f77ade550f3e693707e18f
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-09-10 20:36:29 +0900

    [Silver I] Title: 곱셈, Time: 52 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -5,12 +5,13 @@ import sys
 def remainder(a, b, c):
     if b == 0:
         return 1
-    elif b % 2 == 0:
+
+    elif b%2 == 0:
         half = remainder(a, b//2, c)
         return (half * half) % c
-    else:
-        return (a * remainder(a, b-1, c)) % c
 
+    else:
+        return (a * remainder(a, b-1, c))%c
 
 
 if __name__ == "__main__":
