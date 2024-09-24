49a018bb7a133162eb256535f9c19dea48fc639f
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-09-24 16:32:23 +0900

    [Gold II] Title: 장난감 조립, Time: 116 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.py
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -3,31 +3,17 @@ from collections import defaultdict
 def machine():
     dq = deque()
     base = []
-    not_base = []
 
     for i in range(1, n+1, 1):
         if in_degree[i] == 0:
             dq.append(i)
             base.append(i)
-        else:
-            not_base.append(i)
 
     for b in base:
         part_need[b][b] = 1
 
-
-    result = []
     for _ in range(n):
-
-        #print()
-        #print()
-        #print("part_need: ")
-        #for i in range(1, n + 1, 1):
-        #    print(part_need[i])
-
-
         new_i = dq.popleft()
-        result.append(new_i)
 
         for i, k in adj[new_i]:
             in_degree[i] -= 1
@@ -43,7 +29,6 @@ def machine():
             print("%d %d" %(i, part_need[n][i]))
 
 
-
 if __name__ == "__main__":
     n = int(input())
     m = int(input())
@@ -56,8 +41,5 @@ if __name__ == "__main__":
         x, y, k = map(int, input().split())
         adj[y].append([x, k])
         in_degree[x] += 1
-
-    #print("in_degree :", in_degree)
-    #print("adj :", adj)
 
     machine()
