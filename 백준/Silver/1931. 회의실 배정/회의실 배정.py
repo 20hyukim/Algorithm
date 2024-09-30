37d5953c3160f0102632c65925bc32f44533c450
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-09-30 15:07:08 +0900

    [Silver I] Title: 회의실 배정, Time: 2632 ms, Memory: 53948 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,21 +1,19 @@
 def get_max_meetings(times):
-    # 종료 시간 기준으로 회의를 정렬 (종료 시간이 같을 경우 시작 시간 기준으로 정렬)
     times.sort(key=lambda x: (x[1], x[0]))
+    #print(times)
 
-    # 선택된 회의의 수와 마지막으로 선택된 회의의 종료 시간을 저장할 변수
     count = 0
     last_end_time = 0
 
     for start, end in times:
-        # 현재 회의의 시작 시간이 마지막 회의의 종료 시간보다 크거나 같으면 선택
         if start >= last_end_time:
-            count += 1  # 회의 선택
-            last_end_time = end  # 마지막으로 선택된 회의의 종료 시간 업데이트
+            count += 1
+            last_end_time = end
 
     return count
 
 if __name__ == "__main__":
-    n = int(input())  # 회의의 수 입력
+    n = int(input())
     times = []
     for _ in range(n):
         s, e = map(int, input().split())
