c018f46b68431da247fb5d3d3d9ed681cd343241
미소를뿌리는감자 <20hyukim@naver.com>
2025-10-22 21:03:39 +0900

    [Bronze IV] Title: !밀비 급일, Time: 152 ms, Memory: 15784 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// !밀비 급일
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        while (!str.equals("END")) {
+            String[] parseStr = str.split("");
+
+            for(int i = str.length()-1; i > -1; i--) {
+                System.out.printf("%s", parseStr[i]);
+            }
+            System.out.println();
+
+            str = br.readLine();
+        }
+
+        br.close();
+    }
+
+}
