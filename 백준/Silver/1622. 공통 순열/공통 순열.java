9847a115bd7fbf098197bf0a0402a1eff439561a
미소를뿌리는감자 <20hyukim@naver.com>
2025-11-18 23:44:14 +0900

    [Silver IV] Title: 공통 순열, Time: 112 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Silver/1622. 공통 순열/공통 순열.java
+++ 백준/Silver/1622. 공통 순열/공통 순열.java
@@ -1,39 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String first;
-        
-        while ((first = br.readLine()) != null) {
-            String second = br.readLine();
-            if (second == null) break;
-            
-            TreeMap<Character, Integer> fMap = new TreeMap<>();
-            TreeMap<Character, Integer> sMap = new TreeMap<>();
-            
+        String first = br.readLine();
+        String second = br.readLine();
+        TreeMap<Character, Integer> fMap = new TreeMap<>();
+        TreeMap<Character, Integer> sMap = new TreeMap<>();
+
+        while (first != null) {
+
+            String result = "";
             for (char c : first.toCharArray()) {
                 fMap.put(c, fMap.getOrDefault(c, 0) + 1);
             }
-            
+
             for (char c : second.toCharArray()) {
                 sMap.put(c, sMap.getOrDefault(c, 0) + 1);
             }
-            
-            StringBuilder result = new StringBuilder();
+
             for (Character c : fMap.keySet()) {
                 if (sMap.containsKey(c)) {
-                    int maxDup = Math.min(fMap.get(c), sMap.get(c));
-                    for (int i = 0; i < maxDup; i++) {
-                        result.append(c);
-                    }
+                    int max_dup = Math.min(fMap.get(c), sMap.get(c));
+                    result += Character.toString(c).repeat(max_dup);
                 }
             }
-            
+
             System.out.println(result);
+            fMap.clear();
+            sMap.clear();
+            first = br.readLine();
+            second = br.readLine();
+
         }
     }
 }
