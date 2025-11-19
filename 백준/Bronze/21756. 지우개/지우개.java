37fa88c5ac9ccd039f1b7304b24d1dc0112b1774
미소를뿌리는감자 <20hyukim@naver.com>
2025-11-19 20:52:27 +0900

    [Bronze II] Title: 지우개, Time: 104 ms, Memory: 14368 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/21756. 지우개/지우개.java
+++ 백준/Bronze/21756. 지우개/지우개.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,23 +6,13 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
-        if (n == 1) {
-            System.out.println(1);
-            return;
+        int result = 1;
+        while (result * 2 <= n) {
+            result *= 2;
         }
 
-        int root_n = (int)Math.sqrt(n);
-        int result = (int)Math.pow(2, root_n);
-
-
-        if (result*2 == n) {
-            System.out.println(result * 2);
-            return;
-        }
-
-        System.out.println((int)Math.pow(2, root_n));
+        System.out.println(result);
     }
 }
