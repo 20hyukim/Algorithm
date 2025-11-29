d48e6d31ab95615624e9a4a8ce365a57fa3e80f8
미소를뿌리는감자 <20hyukim@naver.com>
2025-11-29 18:56:26 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18112 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,30 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
-// 백준 1463 : 1로 만들기
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+3];
 
-        int[] dp = new int[n + 1];
-
+        dp[0] = 0;
         dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
 
-        for (int i = 2; i <= n; i++) {
+        int min_cnt = 0;
 
-            dp[i] = dp[i - 1] + 1;
-
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+        for (int i = 4; i < n+1; i++) {
+            min_cnt = i;
+            if (i%2==0) {
+                min_cnt = Math.min(min_cnt, dp[i/2] + 1);
             }
-
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            if (i%3 == 0) {
+                min_cnt = Math.min(min_cnt, dp[i/3] + 1);
             }
+            min_cnt = Math.min(min_cnt, dp[i-1] + 1);
+            dp[i] = min_cnt;
         }
-
+//        System.out.println(Arrays.toString(dp));
         System.out.println(dp[n]);
+
     }
 }
