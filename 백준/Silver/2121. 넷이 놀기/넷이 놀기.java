685aa13d60781b8a792af93d9e44338cdff80484
미소를뿌리는감자 <20hyukim@naver.com>
2025-11-29 22:05:43 +0900

    [Silver III] Title: 넷이 놀기, Time: 1456 ms, Memory: 237192 KB -BaekjoonHub

--- 백준/Silver/2121. 넷이 놀기/넷이 놀기.java
+++ 백준/Silver/2121. 넷이 놀기/넷이 놀기.java
@@ -2,34 +2,37 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
+// 백준 2121번 : 넷이 놀기
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         Set<Point> set = new HashSet<>();
 
-        int points = Integer.parseInt(br.readLine());
-        String[] nm = br.readLine().split(" ");
-        int a = Integer.parseInt(nm[0]);
-        int b = Integer.parseInt(nm[1]);
+        int cases = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < points; i++) {
-            String[] xy = br.readLine().split(" ");
-            int x = Integer.parseInt(xy[0]);
-            int y = Integer.parseInt(xy[1]);
+        String[] ab = br.readLine().split(" ");
+        int a = Integer.parseInt(ab[0]);
+        int b = Integer.parseInt(ab[1]);
 
+
+
+        for (int i = 0; i < cases; i++) {
+            String[] point = br.readLine().split(" ");
+            int x = Integer.parseInt(point[0]);
+            int y = Integer.parseInt(point[1]);
             set.add(new Point(x, y));
         }
 
         int cnt = 0;
-        for (Point p : set) {
-            if (set.contains(new Point(p.x + a, p.y)) &&
-                    set.contains(new Point(p.x+a, p.y + b)) &&
-                    set.contains(new Point(p.x, p.y + b))
-            ) cnt++;
+
+        for(Point p: set) {
+            if (set.contains(new Point(p.x+a, p.y))
+            && set.contains(new Point(p.x, p.y + b))
+            && set.contains(new Point(p.x+a, p.y+b))) {
+                cnt++;
+            }
         }
 
         System.out.println(cnt);
