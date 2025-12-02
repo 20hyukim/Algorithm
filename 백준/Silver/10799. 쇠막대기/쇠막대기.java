fd2fef92b99f957ef88ecababf03b8360680fc69
미소를뿌리는감자 <20hyukim@naver.com>
2025-12-02 21:40:27 +0900

    [Silver II] Title: 쇠막대기, Time: 144 ms, Memory: 16096 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String inps = br.readLine();
+
+        inps = inps.replace("()", "R");
+
+        int len = inps.length();
+
+        int cnt = 0;
+        int overlap = 0;
+        for (int i = 0; i < len; i++) {
+            if (inps.charAt(i) == 'R') {
+                cnt += overlap;
+                continue;
+            }
+            if (inps.charAt(i) == '(') {
+                overlap ++;
+                continue;
+            }
+            cnt ++;
+            overlap --;
+        }
+
+        System.out.println(cnt);
+    }
+}
